a4af28b754a8c466596bed22b3848512ee976fb9
Andrea Mejia <yamejia088@gmail.com>
2025-11-30 14:55:40 -0500

    [UPDATE] DTOs admin / doctor

--- server/src/modules/users/dto/crear-admin.dto.ts
+++ server/src/modules/users/dto/crear-admin.dto.ts
@@ -1,7 +1,8 @@
-import { IsString } from "class-validator";
+import { IsNotEmpty, IsString } from "class-validator";
 import { PersonaBaseDto } from "./persona-base.dto";
 
 export class CrearAdminDto extends PersonaBaseDto {
+  @IsNotEmpty({ message: 'The position is required'})
   @IsString()
   cargo: string; // Ejemplo: "Administrador General"
 }
